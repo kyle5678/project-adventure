cebee8eaaf2feb62aaaeb47be70772dd64e66306
Aiden Kyle Lingad <aklingad@gmail.com>
2020-03-17 16:42:41 +1300

    Initial Commit

---
+++ Project Adventure/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Adventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                try
+                {
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine();
+                }
+        }
+            }
+        }
+    }
